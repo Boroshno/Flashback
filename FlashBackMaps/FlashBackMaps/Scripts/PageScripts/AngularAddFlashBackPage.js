818dcd714b43015becb84b5e790f84326cec1d64
Dmytro Lytvynenko <dmytro.lytvynenko@aviolinx.com>
2016-11-11 23:18:18 +0200

    jsflow settings and connectiong to angular obj

--- FlashBackMaps/FlashBackMaps/Scripts/PageScripts/AngularAddFlashBackPage.js
+++ FlashBackMaps/FlashBackMaps/Scripts/PageScripts/AngularAddFlashBackPage.js
@@ -4,6 +4,9 @@ var AddFlashBackController = function ($scope) {
     $scope.img2 = "";
     $scope.obj = {};
     $scope.existingFlowObject = {};
+    $scope.someHandlerMethod = function (file, event, flow) {
+        alert('s');
+    };
 }
 // The $inject property of every controller (and pretty much every other type of object in Angular) needs to be a string array equal to the controllers arguments, only as strings
 AddFlashBackController.$inject = ['$scope'];
--- FlashBackMaps/FlashBackMaps/Scripts/PageScripts/ImageComparorAddFlashbackPage.js
+++ FlashBackMaps/FlashBackMaps/Scripts/PageScripts/ImageComparorAddFlashbackPage.js
@@ -1,19 +1,19 @@
 ﻿jQuery(document).ready(function ($) {
     //Collect all photos and create forms for them
-    $(".gellery").each(function (index) {
-        var images = $(this).find("img");
-        $('.imgcon').append(' \
-                    <figure class="cd-image-container"> \
-                    <img src="' + images[0].src + '" alt="Original Image"> \
-                        <span class="cd-image-label" data-type="original">Original</span> \
-                        <div class="cd-resize-img"> \
-                        <img src="' + images[1].src + '" alt="Modified Image"> \
-                            <span class="cd-image-label" data-type="modified">Modified</span> \
-                        </div> \
-                        <span class="cd-handle"></span> \
-                    </figure> \
-                            ');
-    });
+    //$(".gellery").each(function (index) {
+    //    var images = $(this).find("img");
+    //    $('.imgcon').append(' \
+    //                <figure class="cd-image-container"> \
+    //                <img src="' + images[0].src + '" alt="Original Image"> \
+    //                    <span class="cd-image-label" data-type="original">Original</span> \
+    //                    <div class="cd-resize-img"> \
+    //                    <img src="' + images[1].src + '" alt="Modified Image"> \
+    //                        <span class="cd-image-label" data-type="modified">Modified</span> \
+    //                    </div> \
+    //                    <span class="cd-handle"></span> \
+    //                </figure> \
+    //                        ');
+    //});
 
     imageSlider($('.cd-image-container'), false, false, false);
 
