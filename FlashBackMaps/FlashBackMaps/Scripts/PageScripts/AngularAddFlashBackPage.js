9d391d87e718ca6b50d8504bb579b7bc428864c7
Dmytro Lytvynenko <dmytro.lytvynenko@aviolinx.com>
2016-11-11 22:54:36 +0200

    flowjs setup

--- FlashBackMaps/FlashBackMaps/Scripts/PageScripts/AngularAddFlashBackPage.js
+++ FlashBackMaps/FlashBackMaps/Scripts/PageScripts/AngularAddFlashBackPage.js
@@ -1,8 +1,9 @@
 ﻿(function () {
 var AddFlashBackController = function ($scope) {
-    $scope.models = {
-        helloAngular: 'I work!'
-    };
+    $scope.img1 = "";
+    $scope.img2 = "";
+    $scope.obj = {};
+    $scope.existingFlowObject = {};
 }
 // The $inject property of every controller (and pretty much every other type of object in Angular) needs to be a string array equal to the controllers arguments, only as strings
 AddFlashBackController.$inject = ['$scope'];
